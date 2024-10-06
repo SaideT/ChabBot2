a969df00a75747d9b8c789a20fd704686d31585e
Saide <50343932+SaideT@users.noreply.github.com>
2024-10-06 17:58:32 -0400

    Update streamlit_app.py with Fei Han's version

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,40 @@
 import streamlit as st
 import openai
 
-st.set_page_config(page_title="Chat GPT3.5", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
-st.title("Chat with the Tax Assistant 2022, powered by GPT3.5")
+st.set_page_config(page_title="Chatbot powered by GPT-3.5 Turbo", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+st.title("Chatbot powered by GPT-3.5 Turbo")
 
 # Sidebar for entering OpenAI key
 with st.sidebar:
-    st.title('OpenAI key')
-    openai_key = st.text_input('Enter OpenAI key:', type='password')
-    if not openai_key:
-        st.warning('Please enter your OpenAI key!', icon='⚠️')
+    st.title('Enter OpenAI API key here:')
+    if 'openai_key' in st.secrets:
+        st.success('OpenAI key already provided!', icon='✅')
+        openai_key = st.secrets['openai_key']
     else:
-        st.success('Proceed to entering your prompt message!', icon='👉')
+        openai_key = st.text_input('Enter OpenAI API key here:', type='password', label_visibility="collapsed")
+        if not openai_key:
+            st.warning('Please enter your OpenAI key!', icon='⚠️')
+        else:
+            st.success('Proceed to entering your prompt message!', icon='👉')
 
- # Store chat messages, and initialize the chat message history
-if "messages" not in st.session_state.keys():
-    st.session_state.messages = [{"role": "assistant", "content": "Ask me a question about the 2022 tax return filing!"}]
+# Store chat messages, and initialize the chat message history
+if 'messages' not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "I am your AI assistant. Ask me something!"}]
+
+# Display the prior chat messages
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"].replace("$","\$"))
 
 # User-provided prompt
 if prompt := st.chat_input(disabled=not openai_key):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
-        st.write(prompt)
+        st.write(prompt.replace("$","\$"))
 
 openai.api_key = openai_key
 
-# Function to get the GPT3.5's response
+# Function to get the GPT-3.5's response
 def get_assistant_response(messages):
     r = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
@@ -34,16 +43,11 @@ def get_assistant_response(messages):
     response = r.choices[0].message.content
     return response
 
-# Display the prior chat messages
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
-
-# If last message is not from assistant, generate a new response
+# If the last message is not from assistant, generate a new response
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
             response = get_assistant_response(st.session_state.messages)
-            st.write(response)
-    message = {"role": "assistant", "content": response}
-    st.session_state.messages.append(message) # Add response to message history
+            st.write(response.replace("$","\$"))
+    # Add response to chat messages        
+    st.session_state.messages.append({"role": "assistant", "content": response}) 
